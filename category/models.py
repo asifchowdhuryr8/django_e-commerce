5f6cf4b1903841aeded971e31ebf89b782b3e270
ITS MAC <asifchowdhuryrafi143@outlook.com>
2021-11-18 08:13:58 +0600

    Created category model and learned how to manually pluralize django model name

--- category/models.py
+++ category/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=100, unique=True)
+    description = models.TextField(max_length=250, null=True, blank=True)
+    image = models.ImageField(
+        upload_to='images/categories', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        # By default python convert model name to lowercase and pluralize it. Here I have used category as the model name and django convert in to categorys with an additional s to make it plural. So if you want to manually change the plural model name then use above code.
