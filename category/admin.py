82eab21af02bc4ddb59bc77c3596786ab2cf4563
ITS MAC <asifchowdhuryrafi143@outlook.com>
2021-11-19 08:18:50 +0600

    Added automatically fill slug field value accordingly category name functionality

--- category/admin.py
+++ category/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Category
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    # slug field will be automatically populated with the value of the name field
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name', 'slug']
+
+
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
--- category/models.py
+++ category/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    slug = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField(max_length=250, null=True, blank=True)
     image = models.ImageField(
         upload_to='images/categories', null=True, blank=True)
