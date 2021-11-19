ef6bc7049da254bbb5c406146841aff6025f7232
ITS MAC <asifchowdhuryrafi143@outlook.com>
2021-11-19 08:02:38 +0600

    Added media file configuration

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -127,6 +127,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+# Media files(user uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'static/media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('category.urls')),
     path('admin/', admin.site.urls),
     path('category/', include('category.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
