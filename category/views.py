fec5ba78f4a0ed0c0882a6c6c628b56286cfd1ae
ITS MAC <asifchowdhuryrafi143@outlook.com>
2021-11-19 09:19:31 +0600

    Rendering available products to the home page

--- category/views.py
+++ category/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from store.models import Product
 
 # Create your views here.
 
 def homepage(request):
-    return render(request, 'index.html')
+    products = Product.objects.all().filter(is_available=True)
+    context= {'products':products}
+    return render(request, 'index.html', context)
