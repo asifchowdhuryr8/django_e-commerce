72c700655c7ec4bc24b26fd62f8d0c8108dc9c99
ITS MAC <asifchowdhuryrafi143@outlook.com>
2021-11-28 10:45:36 +0600

    Completed the product variation functionality

--- cart/urls.py
+++ cart/urls.py
@@ -7,8 +7,8 @@ app_name = "cart"
 urlpatterns = [
     path("", views.cart, name="cart"),
     path("add_card/<int:product_id>/", views.add_cart, name="add_cart"),
-    path("decrease_cart_item_quantity/<int:product_id>/",
+    path("decrease_cart_item_quantity/<int:product_id>/<int:cart_item_id>/",
          views.decrease_cart_item_quantity, name="decrease_cart_item_quantity"),
-    path("remove_cart_item/<int:product_id>/",
+    path("remove_cart_item/<int:product_id>/<int:cart_item_id>/",
          views.remove_cart_item, name="remove_cart_item"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -40,43 +40,68 @@ def add_cart(request, product_id):
             cart_id=get_or_set_session_id(request))   # create a new cart with the session id as cart id
         cart.save()   # save the cart to the database
 
-    try:
+    is_cart_item_exist = CartItem.objects.filter(
+        product=product, cart=cart).exists()
+    if is_cart_item_exist:
         # get the cart item if the product is already in the cart
-        cart_item = CartItem.objects.get(product=product, cart=cart)
-        cart_item.quantity += 1  # update the product quantity of the cart item
-        cart_item.save()    # save the cart item to the database
+        cart_item = CartItem.objects.filter(product=product, cart=cart)
+        existing_variation_item_list = []
+        existing_variation_item_id = []
+        for item in cart_item:
+            """get all the variations of the cart item and their ids"""
+            existing_variation_item = item.variation.all()
+            existing_variation_item_list.append(list(existing_variation_item))
+            existing_variation_item_id.append(item.id)
 
-    except CartItem.DoesNotExist:   # if the product is not in the cart create a new cart item with the product and set quantity 1 as well as save it to the database
+        if product_variation in existing_variation_item_list:
+            """If a user has already bought a product of this same category then update the quantity. e.g. user bought a shirt (blue+40) then if he want to buy another then update the quantity from the previous cart item"""
+            index = existing_variation_item_list.index(
+                product_variation)   # get the index of the number
+            item_id = existing_variation_item_id[index]  # get the id of the cart item
+            cart_item = CartItem.objects.get(
+                product=product, id=item_id)   # get the cart item
+            cart_item.quantity += 1
+            cart_item.save()
+    else:
+        #     """If a user buy a new product or buy the same product with another category then add it as a new cart item. e.g. user adds  a shirt (blue+40) to his cart and again adds a new shirt (black+30) to his cart. In this case add two item in the cart"""
         cart_item = CartItem.objects.create(
-            product=product,
-            quantity=1,
-            cart=cart
-        )
+            product=product, quantity=1, cart=cart)
+        if len(product_variation) > 0:
+            cart_item.variation.clear()   # clear the previously selected variations
+            # add all the product variations
+            cart_item.variation.add(*product_variation)
         cart_item.save()
     return redirect('cart:cart')    # redirect to the cart page
 
 
-def decrease_cart_item_quantity(request, product_id):
+def decrease_cart_item_quantity(request, product_id, cart_item_id):
     cart = Cart.objects.get(
         cart_id=get_or_set_session_id(request))  # get the cart
     # get the product and if it doesn't exist return 404 error
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(
-        product=product, cart=cart)    # get the cart item
-    if cart_item.quantity > 1:  # if the quantity is greater than 1 then decrement the quantity
-        cart_item.quantity -= 1
-        cart_item.save()
-    else:
-        cart_item.delete()  # if the quantity is 1 then delete the cart item
+    try:
+        cart_item = CartItem.objects.get(
+            product=product, cart=cart, id=cart_item_id)    # get the cart item
+        if cart_item.quantity > 1:  # if the quantity is greater than 1 then decrement the quantity
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()  # if the quantity is 1 then delete the cart item
+    except ObjectDoesNotExist:
+        pass
     return redirect('cart:cart')    # redirect to the cart page
 
 
-def remove_cart_item(request, product_id):
+def remove_cart_item(request, product_id, cart_item_id):
     """Delete the cart item if user clicks the delete button from cart page"""
     cart = Cart.objects.get(cart_id=get_or_set_session_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)
-    cart_item.delete()
+    try:
+        cart_item = CartItem.objects.get(
+            product=product, cart=cart, id=cart_item_id)
+        cart_item.delete()
+    except ObjectDoesNotExist:
+        pass
     return redirect('cart:cart')
 
 
