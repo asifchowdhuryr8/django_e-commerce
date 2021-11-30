04f647ac23379eb4e6000b9662b889c25b7dfe2d
ITS MAC <asifchowdhuryrafi143@outlook.com>
2021-11-30 21:08:49 +0600

    Added login and logout functionality only for superuser/admin. Read the comment of views.py file of account application you'll understand why I have done this.

--- account/views.py
+++ account/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect, render
 from .forms import RegistrationForm
 from .models import Account
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -29,3 +30,37 @@ def register(request):
         form = RegistrationForm()
     context = {'form': form}
     return render(request, "account/register.html", context)
+
+
+def login(request):
+    if request.user.is_authenticated:
+        # Redirect to homepage if user is already logged in
+        return redirect('category:homepage')
+
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:    # If user is not None, then the user exists in the database
+            auth.login(request, user)
+            return redirect('category:homepage')
+        else:   # If user is None, then the user does not exist in the database
+            messages.warning(request, 'Invalid credentials')
+            return redirect('account:login')
+    return render(request, "account/login.html")
+
+
+# This decorator is used to prevent users from accessing the logout page if they are not logged in
+@login_required(login_url='account:login')
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'You have successfully logged out!')
+    return redirect('account:login')
+
+
+"""
+Currently, Only super user can log in. Normal user cannot log in because they haven't activated their account through the email verification which will be implemented in the future.
+
+When creating the account model for normal user I have set the is_active field to False and it'll be become True when the user clicks on the link sent to their email.
+"""
